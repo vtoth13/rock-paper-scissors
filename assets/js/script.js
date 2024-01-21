ac18411858b45b6c163d56c9dfb6a28dd27c6bc2
vtoth13 <snapviki13@gmail.com>
2024-01-21 15:16:58 +0100

    Fixed: moves shown player name

--- assets/js/script.js
+++ assets/js/script.js
@@ -94,7 +94,7 @@ function makeMove(playerMove) {
     //Display the chose move in the UI
     const movesElement = document.querySelector('.js_moves_chosen');
     movesElement.innerHTML = `
-    You <img src="assets/images/${playerMove}_emoji.png" class="move_icon">
+    ${playerName} <img src="assets/images/${playerMove}_emoji.png" class="move_icon">
     |
     <img src="assets/images/${computerMove}_emoji.png" class="move_icon"> Computer
     `;

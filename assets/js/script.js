f3dbfa8ab5bd6cf4b3776add055cf0b8fd94a497
vtoth13 <snapviki13@gmail.com>
2024-01-21 14:25:45 +0100

    Add: comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,43 @@
 /* jshint esversion: 8 */
 
+//Initalize the score object to keep track of wins, losses and ties
 let score = {
     wins: 0,
     losses: 0,
     ties: 0
 };
 
+//Player's name variable
 let playerName;
 
-let roundCount = 0;
-
+//Event listener to open popup
 document.getElementById('openPopup').addEventListener('click', function () {
     document.getElementById('overlay').style.display = 'block';
     document.getElementById('popup').style.display = 'block';
     document.addEventListener('keydown', handleKeyPress);
 });
 
+//Event listener to close popup
 document.getElementById('closePopup').addEventListener('click', closePopup);
 
+//Event listener to close popup when clicking overlay
 document.getElementById('overlay').addEventListener('click', closePopup);
 
+//Function to close popup
 function closePopup() {
     document.getElementById('overlay').style.display = 'none';
     document.getElementById('popup').style.display = 'none';
     document.removeEventListener('keydown', handleKeyPress);
 };
 
+//Function to handle key press Esc
 function handleKeyPress(event) {
     if (event.keyCode === 27) {
         closePopup();
     }
 };
 
+//Event listener for rock, paper and scissors button
 document.getElementById('rockButton').addEventListener('click', function () {
     makeMove('rock');
 });
@@ -44,22 +50,28 @@ document.getElementById('scissorsButton').addEventListener('click', function () 
     makeMove('scissors');
 });
 
+//Load the saved score from localStorage
 const savedScore = JSON.parse(localStorage.getItem('score'));
 if (savedScore) {
     score = savedScore;
 }
 
+//Update score element in the UI
 updateScoreElement();
 
+//Function to handle player's move
 function makeMove(playerMove) {
 
+    //Prompt for player's name
     if (!playerName) {
         playerName = prompt("Please enter your name:")
     }
 
+    //Get the computer's move
     const computerMove = pickComputerMove();
     const resultElement = document.querySelector('.js_result')
 
+    //Determine the result of the round
     if (playerMove === computerMove) {
         resultElement.innerHTML = 'Tie';
         score.ties += 1;
@@ -76,23 +88,27 @@ function makeMove(playerMove) {
         score.losses += 1;
     }
 
+    //Increment the round count
     roundCount++;
 
+    //Display the chose move in the UI
     const movesElement = document.querySelector('.js_moves_chosen');
     movesElement.innerHTML = `
     You <img src="assets/images/${playerMove}_emoji.png" class="move_icon">
     <img src="assets/images/${computerMove}_emoji.png" class="move_icon"> Computer
     `;
 
+    //Update the score element in the UI
     updateScoreElement();
 
     if (roundCount === 10) {
         showWinner();
     }
-
+    //Remove the score from localStorage
     localStorage.removeItem('score');
 }
 
+//Function to show overall winner
 function showWinner() {
     const resultElement = document.getElementById('winner');
     const popupResult = document.getElementById('popupResult');
@@ -120,6 +136,7 @@ function resetScore() {
     localStorage.removeItem('score');
 }
 
+//Function to randomly pick the computer's move
 function pickComputerMove() {
     const randomNumber = Math.random();
     let computerMove = 'rock';
@@ -135,6 +152,7 @@ function pickComputerMove() {
     return computerMove;
 }
 
+//Function to update the score element
 function updateScoreElement() {
     document.querySelector('.js_score').innerHTML = `
     Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}
